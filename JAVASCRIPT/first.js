2b0576b3f0c3f62d2e4341887752f4f291f7ff26
Subham-Suar <subhamsuar911609@gmail.com>
2025-07-02 08:54:01 +0530

    all edited file

---
+++ JAVASCRIPT/first.js
@@ -0,0 +1,9 @@
+let game_name = 25;
+let user_name =prompt("Guess the number");
+console.log(user_name);
+while(user_name!=game_name)
+{
+    user_name=prompt("you entered the wrong number");
+
+}
+console.log("congratulation you enter the correct value")
---
+++ react.js/vite-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './App'
+const App =()=>{
+  return(
+    <div>
+    <h1>vite app</h1>
+     <h1>vite app</h1>
+      <h1>vite app</h1>
+       <h1>vite app</h1>
+  </div>
+  )
+};
+
+export default App
