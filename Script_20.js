5e9375a1f4ed86ae7099168852e9ec4e855f8036
Subham-Suar <subhamsuar911609@gmail.com>
2025-11-06 20:06:27 +0530

    Global scope & block scope

---
+++ Script_20.js
@@ -0,0 +1,16 @@
+function calculateCartNumber(...num){
+    return   num;
+}
+
+//console.log(calculateCartNumber(200,300,400,500,600));
+
+let a =20;
+
+if(true){
+    let a =22
+    const b =33
+    var c =44
+    console.log("inner:", a)
+}
+
+console.log("outer:" ,a ) // in-side curly brashes {  block scope  }
