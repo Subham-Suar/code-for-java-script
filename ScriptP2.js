9781705bf3b4bf87b65822c940ca6ae579ca1fb1
Subham-Suar <subhamsuar911609@gmail.com>
2025-11-18 20:53:12 +0530

    Destructuring

--- ScriptP2.js
+++ ScriptP2.js
@@ -1,15 +1,42 @@
-const user= {
-    name :"Bob",
-    age:13,
-    section:"Genius"
+// const user= {
+//     name :"Bob",
+//     age:13,
+//     section:"Genius"
+// }
+
+// //console.log(user);
+
+// // {name} = user ;
+// // ({ name } = user);
+// // Because parentheses prevent JavaScript from interpreting {} as a block.
+
+// const {name:userName} = user ;
+// //console.log(userName);
+
+// const user = {
+//   name: "Bob",
+//   age: 13
+// };
+
+// let { age, ...rest } = user;
+// age = 20;
+
+// const newUser = { ...rest, age };
+
+//console.log(newUser);
+// { name: "Bob", age: 20 }
+
+const shop ={
+    profit:13,
+    loss: 16,
+    vendor:"Subham"
 }
 
-//console.log(user);
+ let {vendor} = shop ;
+ //console.log("After destructuring",vendor);
+  vendor = "Saswat";
+//console.log("After destructuring with rename",vendor);
 
-// {name} = user ;
-// ({ name } = user);
-// Because parentheses prevent JavaScript from interpreting {} as a block.
-
-const {name:userName} = user ;
-console.log(userName);
+const newShop = {...sho}
+console.log(newShop);
 
