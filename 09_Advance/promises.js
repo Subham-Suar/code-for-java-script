5da6d55f6a537439da0f347c9695a53ce5b986b4
Subham-Suar <subhamsuar911609@gmail.com>
2026-01-11 16:46:09 +0530

    api request

--- 09_Advance/promises.js
+++ 09_Advance/promises.js
@@ -8,13 +8,58 @@
 // promiseOne.then(function(){
 //     console.log("promise resolved")
 //})
-const promiseTwo = new Promise(function(resolve,reject){
-    setTimeout(function(){
-        console.log("Async task done")
-    },100)
-    resolve({userName:"Hitesh", userMail:"Subham@ex.com"});
-});
+// const promiseTwo = new Promise(function(resolve,reject){
+//     let error = false;
+//     if(!error){
+//         resolve({userName:"Subham",status:"logged in"})
+//     }else{
+//         reject("ERROR! Something Went Wrong");
+//     }
+// });
 
-promiseTwo.then(function(user){
-    console.log(user);
-})
+//Consume promise
+
+// promiseTwo.then(function(user){
+//     console.log(user)
+//     return user.userName
+// }).then((userName)=>{
+//     console.log(userName)
+// }).catch((error)=>{
+//     console.log(error)
+// })
+
+//using Async and await
+
+// async function consumePromiseTwo(){
+//     const response = await promiseTwo;
+//     console.log(response);
+// }
+
+// consumePromiseTwo()
+
+async function getAllUser() {
+    try {
+        const response = await fetch('https://api.github.com/users/Subham-Suar');
+        const data = await response.json()
+        console.log(data);
+
+    } catch (error) {
+        console.log("Error !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", error);
+    }
+}
+
+getAllUser();
+
+
+// async function fettchEx() {
+//     try {
+//         const response = await fetch('https://api.github.com/users/Subham-Suar')
+//        const data = await response.json()
+//         console.log(data)
+//     }
+//     catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// fettchEx()
