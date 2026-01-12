12acfed8c52f79bec21d5e254f18375cb6f8b2c8
Subham-Suar <subhamsuar911609@gmail.com>
2026-01-12 16:55:47 +0530

    oop in js

---
+++ 09_Advance/oops.js
@@ -0,0 +1,12 @@
+function User(username ,userid ,loggedIn){
+    this.username =username;
+    this.usserid =userid;
+    this.loggedIn = loggedIn;
+
+    return this;
+}
+
+const userOne = new User("Subham",18,true);
+console.log(userOne)
+
+console.log(userOne instanceof User)
